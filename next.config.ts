d5a62dbf95f8078da5a3c14e0c494f893c9b67fa
조연경 <sig.yg5057@gmail.com>
2025-11-28 08:46:19 +0900

    chore: 배포 환경 CORS 문제 해결(next.config.ts 수정)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: "https://recruit.paysbypays.com/api/v1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
